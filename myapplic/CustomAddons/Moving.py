6009720ca99f8987c62d2b2b012ba9aa04f1d48e
Maksim Kazachenka <minskk1@list.ru>
2019-04-25 11:59:21 +0300

    Move imitation action and scroll action into CustomAddons package Add debug timings Rename GoogleSite to pzzBy

---
+++ myapplic/CustomAddons/Moving.py
@@ -0,0 +1,31 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.expected_conditions import *
+from selenium.webdriver.support.wait import WebDriverWait
+from selene.api import *
+
+
+class moving:
+
+    @staticmethod
+    def scroll_shim(passed_in_driver, object):
+        x = object.location['x']
+        y = object.location['y']
+        scroll_by_coord = 'window.scrollTo(%s,%s);' % (
+            x,
+            y
+        )
+        scroll_nav_out_of_way = 'window.scrollBy(0, -120);'
+        passed_in_driver.execute_script(scroll_by_coord)
+        passed_in_driver.execute_script(scroll_nav_out_of_way)
+
+    @staticmethod
+    def action_click_to_element(driver, element, xpath=None):
+        action_chains = ActionChains(driver)
+        moving.scroll_shim(driver, element)
+        if xpath is not None:
+            WebDriverWait(
+                driver, config.timeout).until(
+                    element_to_be_clickable(
+                        by.xpath(
+                            xpath)))
+        action_chains.move_to_element(element).click().perform()
--- myapplic/MainPackage/__init__.py
+++ myapplic/MainPackage/__init__.py
@@ -3,20 +3,20 @@ from selene import config, browser
 from selene.browsers import BrowserName
 from selenium import webdriver
 
-from myapplic.Tests.GoogleSite import GoogleSite
+from myapplic.Tests.GoogleSite import pzzBy
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return GoogleSite.run_test(setup_browser())
+    return pzzBy.run_test(setup_browser())
 
 
 def setup_browser():
     driver = webdriver.Firefox()
     browser.set_driver(driver)
-    config.timeout = 2
+    config.timeout = 5
     return driver
 
 
